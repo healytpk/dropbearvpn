ae74305ccc61ac35b762b19da1b81e6ebcadab4a
healytpk <thomaspkhealy@yahoo.com>
2023-01-22 01:00:59 +0000

    Add new file for creating and opening tun device on Linux

---
+++ cli-vpn-linux-tun.c
@@ -0,0 +1,49 @@
+#include <string.h>          // strncpy, memset
+#include <sys/ioctl.h>       // ioctl
+#include <fcntl.h>           // open
+#include <linux/if.h>        // ifreq
+#include <linux/if_tun.h>    //
+#include <unistd.h>          // close
+
+int tun_alloc(char *const dev, int const flags)
+{
+    struct ifreq ifr = {0};
+    int fd, err;
+    char *const clonedev = "/dev/net/tun";
+
+    /* Arguments taken by the function:
+    *
+    * char *dev: the name of an interface (or '\0'). MUST have enough
+    *   space to hold the interface name if '\0' is passed
+    * int flags: interface flags (eg, IFF_TUN etc.)
+    */
+
+    /* open the clone device */
+    if ( (fd = open(clonedev, O_RDWR)) < 0 ) return fd;
+
+    ifr.ifr_flags = flags;   /* IFF_TUN or IFF_TAP, plus maybe IFF_NO_PI */
+
+    if ( '\0' != dev[0] )
+    {
+        /* if a device name was specified, put it in the structure; otherwise,
+        * the kernel will try to allocate the "next" device of the
+        * specified type */
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+
+    /* try to create the device */
+    if ( (err = ioctl(fd, TUNSETIFF, &ifr)) < 0 )
+    {
+        close(fd);
+        return err;
+    }
+
+    /* if the operation was successful, write back the name of the
+    * interface to the variable "dev", so the caller can know
+    * it. Note that the caller MUST reserve space in *dev       */
+    strcpy(dev, ifr.ifr_name);
+
+    /* this is the special file descriptor that the caller will use to talk
+    * with the virtual interface */
+    return fd;
+}
